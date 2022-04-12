46097f65b6b43fe4215593771f1754aab974de87
jaju1212 <80427858+jaju1212@users.noreply.github.com>
2022-04-12 18:57:59 +0200

    fix

---
+++ esp32_hydroponics/src/ciotc_config.h
@@ -0,0 +1,83 @@
+/******************************************************************************
+ * Copyright 2018 Google
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *****************************************************************************/
+// This file contains your configuration used to connect to Cloud IoT Core
+
+// Wifi network details.
+const char *ssid = "SSID";
+const char *password = "PASSWORD";
+
+// Cloud iot details.
+const char *project_id = "project-id";
+const char *location = "us-central1";
+const char *registry_id = "my-registry";
+const char *device_id = "my-esp32-device";
+
+// Configuration for NTP
+const char* ntp_primary = "pool.ntp.org";
+const char* ntp_secondary = "time.nist.gov";
+
+#ifndef LED_BUILTIN
+#define LED_BUILTIN 13
+#endif
+
+// To get the private key run (where private-key.pem is the ec private key
+// used to create the certificate uploaded to google cloud iot):
+// openssl ec -in <private-key.pem> -noout -text
+// and copy priv: part.
+// The key length should be exactly the same as the key length bellow (32 pairs
+// of hex digits). If it's bigger and it starts with "00:" delete the "00:". If
+// it's smaller add "00:" to the start. If it's too big or too small something
+// is probably wrong with your key.
+const char *private_key_str =
+    "6e:b8:17:35:c7:fc:6b:d7:a9:cb:cb:49:7f:a0:67:"
+    "63:38:b0:90:57:57:e0:c0:9a:e8:6f:06:0c:d9:ee:"
+    "31:41";
+
+// Time (seconds) to expire token += 20 minutes for drift
+const int jwt_exp_secs = 60*20; // Maximum 24H (3600*24)
+
+// To get the certificate for your region run:
+//   openssl s_client -showcerts -connect mqtt.googleapis.com:8883
+// for standard mqtt or for LTS:
+//   openssl s_client -showcerts -connect mqtt.2030.ltsapis.goog:8883
+// Copy the certificate (all lines between and including ---BEGIN CERTIFICATE---
+// and --END CERTIFICATE--) to root.cert and put here on the root_cert variable.
+
+// Alternatively, get Google's minimal root CA set for mqtt.2030.ltsapis.goog.
+// https://cloud.google.com/iot/docs/how-tos/mqtt-bridge#downloading_mqtt_server_certificates
+//   wget https://pki.goog/gtsltsr/gtsltsr.crt
+//   openssl x509 -inform DER -in gtsltsr.crt -out primary.pem -text
+
+const char *root_cert =
+    "-----BEGIN CERTIFICATE-----\n"
+    "MIIBxTCCAWugAwIBAgINAfD3nVndblD3QnNxUDAKBggqhkjOPQQDAjBEMQswCQYD\n"
+    "VQQGEwJVUzEiMCAGA1UEChMZR29vZ2xlIFRydXN0IFNlcnZpY2VzIExMQzERMA8G\n"
+    "A1UEAxMIR1RTIExUU1IwHhcNMTgxMTAxMDAwMDQyWhcNNDIxMTAxMDAwMDQyWjBE\n"
+    "MQswCQYDVQQGEwJVUzEiMCAGA1UEChMZR29vZ2xlIFRydXN0IFNlcnZpY2VzIExM\n"
+    "QzERMA8GA1UEAxMIR1RTIExUU1IwWTATBgcqhkjOPQIBBggqhkjOPQMBBwNCAATN\n"
+    "8YyO2u+yCQoZdwAkUNv5c3dokfULfrA6QJgFV2XMuENtQZIG5HUOS6jFn8f0ySlV\n"
+    "eORCxqFyjDJyRn86d+Iko0IwQDAOBgNVHQ8BAf8EBAMCAYYwDwYDVR0TAQH/BAUw\n"
+    "AwEB/zAdBgNVHQ4EFgQUPv7/zFLrvzQ+PfNA0OQlsV+4u1IwCgYIKoZIzj0EAwID\n"
+    "SAAwRQIhAPKuf/VtBHqGw3TUwUIq7TfaExp3bH7bjCBmVXJupT9FAiBr0SmCtsuk\n"
+    "miGgpajjf/gFigGM34F9021bCWs1MbL0SA==\n"
+    "-----END CERTIFICATE-----\n";
+
+// In case we ever need extra topics
+const int ex_num_topics = 0;
+const char* ex_topics[ex_num_topics];
+//const int ex_num_topics = 1;
+//const char* ex_topics[ex_num_topics] = {
+//  "/devices/my-device/tbd/#"
+//};
