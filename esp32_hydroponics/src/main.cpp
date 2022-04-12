b78910069fd81289b1cd913d2398560f9f63846c
jaju1212 <80427858+jaju1212@users.noreply.github.com>
2022-04-12 15:28:25 +0200

    update

--- esp32_hydroponics/src/main.cpp
+++ esp32_hydroponics/src/main.cpp
@@ -4,28 +4,32 @@
 #include "secrets.h"
 #include <ESP32TimerInterrupt.h>
 
-
 // Pins
-int DHT_PIN = 2;
+#define OLED_CLOCK 20
+#define OLED_DATA 21
+
+int dht_pin = 2;
+
 int clock = 21;
 int data = 20;
 
 struct States{
   uint8_t led;
   uint8_t water;
-  uint8_t fan; 
-  uint8_t air;
+  uint8_t fan;
+  uint8_t pump;
 };
 
 States relay;
+States pwm;
 
 float temperature = 0;
 float humidity = 0;
 int DHTerror = SimpleDHTErrSuccess;
 
 // konstruktory
-SimpleDHT22 dht22(DHT_PIN);
-U8G2_SSD1306_128X32_UNIVISION_F_SW_I2C u8g2(U8G2_R0, clock, data, /* reset=*/ U8X8_PIN_NONE);
+SimpleDHT22 dht22(dht_pin);
+U8G2_SSD1306_128X32_UNIVISION_F_SW_I2C u8g2(U8G2_R0, OLED_CLOCK, OLED_DATA, /* reset=*/ U8X8_PIN_NONE);
 
 void setup() {
   u8g2.setFont(u8g2_font_ncenB08_tr);
@@ -34,9 +38,8 @@ void setup() {
 
 void loop() {
 
+
 }
-
-
 
 int DHT_getTemp(void)
 {
