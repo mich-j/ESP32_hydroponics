b273cada5b54ecbdeddd0a7cdea7cc299160c627
jaju1212 <80427858+jaju1212@users.noreply.github.com>
2022-04-16 18:17:24 +0200

    preparation for sending telemetry

--- esp32_hydroponics/src/main.cpp
+++ esp32_hydroponics/src/main.cpp
@@ -1,38 +1,58 @@
+#define _TIMERINTERRUPT_LOGLEVEL_     4
+#define TIMER_INTERRUPT_DEBUG      0
+
 #include <Arduino.h>
-#include "ESP32TimerInterrupt.h"
+#include "ESP32_New_TimerInterrupt.h"
 #include <SimpleDHT.h>
 #include <U8g2lib.h>
 #include "esp32-mqtt.h"
+#include <ArduinoJson.h>
+#include "ESP32_New_ISR_Timer.h"
 
-#define _TIMERINTERRUPT_LOGLEVEL_ 4
 
+//#define _TIMERINTERRUPT_LOGLEVEL_ 4
+#define WATER_OK 1
+#define WATER_LOW 0
 
 // Pins
 #define OLED_CLOCK_PIN 20
 #define OLED_DATA_PIN 21
 #define WATER_PUMP_PIN 34
+#define WATER_SENSOR_PIN 14
+#define LED_PIN 5
 #define FAN_PIN 35
 #define AIR_PIN 36
 
-#define TIM0_INTERVAL 3000
-#define TIMER0_DURATION_MS        5000
+#define TIMER0_INTERVAL_MS 3000
+#define TIMER0_DURATION_MS 5000
+
 
 int dht_pin = 2;
 
-struct States
+struct Device
 {
-  uint8_t led;
-  uint8_t air;
-  uint8_t fan;
-  uint8_t pump;
+  struct relays
+  {
+    uint8_t pin;
+    uint8_t state;
+  };
+  relays led;
+  relays air;
+  relays fan;
+  relays pump;
+  relays water_level;
+
 };
 
-States relay; // struktura przechowująca stany przekaźników
-States pwm;
+
+
+Device dev;
 
 float temperature = 0;
 float humidity = 0;
 int DHTerror = SimpleDHTErrSuccess;
+
+char buf[100];
 
 // konstruktory
 SimpleDHT22 dht22(dht_pin);
@@ -40,9 +60,17 @@ U8G2_SSD1306_128X32_UNIVISION_F_SW_I2C u8g2(U8G2_R0, OLED_CLOCK_PIN, OLED_DATA_P
 
 ESP32Timer ITimer0(0);
 
+StaticJsonDocument<200> doc;
+
 // Function declarations
 void DHT_getTempHum(void);
-bool IRAM_ATTR TimerHandler0(void *timerNo);
+
+
+bool IRAM_ATTR TimerHandler0(void *timerNo)
+{
+
+  return true;
+}
 
 void setup()
 {
@@ -50,20 +78,31 @@ void setup()
   u8g2.setFont(u8g2_font_ncenB08_tr);
   setupCloudIoT();
 
-  ITimer0.attachInterruptInterval(TIM0_INTERVAL, TimerHandler0);
+  ITimer0.attachInterruptInterval(TIMER0_INTERVAL_MS, TimerHandler0);
+
+  
 }
 
 void loop()
 {
-
   DHT_getTempHum();
+  GetWaterLevel();
+  
+
+  //data for publishing
+  doc["temp"] = temperature;
+  doc["hum"] = humidity;
+  doc["fan"] = dev.fan.state;
+  doc["led"] = dev.led.state;
+  doc["air"] = dev.air.state;
+  doc["pump"] = dev.pump.state;
+  doc["waterlevel"] = dev.water_level.state;
+  
+  serializeJson(doc, buf);
+  publishTelemetry(buf);
 }
 
-bool IRAM_ATTR TimerHandler0(void *timerNo)
-{
 
-  return true;
-}
 
 void DHT_getTempHum(void)
 {
@@ -73,19 +112,33 @@ void DHT_getTempHum(void)
     u8g2.print(SimpleDHTErrCode(DHTerror));
     u8g2.sendBuffer();
   }
+
 }
 
-bool CheckStatesDiff(States curr, States prevy){
-  int *curr_ptr;
-  int *prevy_ptr;
+// bool CheckStatesDiff(States curr, States prevy)
+// {
+//   int *curr_ptr;
+//   int *prevy_ptr;
 
-  bool is_diff = false;
+//   bool is_diff = false;
 
-  for(int i = 0; i<3; i++){
-    if(*(curr_ptr+i) != *(prevy_ptr+i)){
-      is_diff=true;
-    }
-  }
+//   for (int i = 0; i < 3; i++)
+//   {
+//     if (*(curr_ptr + i) != *(prevy_ptr + i))
+//     {
+//       is_diff = true;
+//     }
+//   }
 
-  return is_diff;
+//   return is_diff;
+// }
+
+void SetRelay(uint8_t pin, bool state)
+{
+  digitalWrite(pin, state);
+
+}
+
+uint8_t GetWaterLevel(void){
+  dev.water_level.state = digitalRead(WATER_SENSOR_PIN);
 }
