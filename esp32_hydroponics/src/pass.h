cc2acc92fcc3f030ea2e550aa57537a96e279b82
mich-j <80427858+mich-j@users.noreply.github.com>
2022-05-23 23:36:55 +0200

    Delete pass.h

--- esp32_hydroponics/src/pass.h
+++
@@ -1,9 +0,0 @@
-#define WIFI_SSID "Ecuador"
-#define WIFI_PASSWORD "slodkiekotki69"
-// #define WIFI_SSID "getto"
-// #define WIFI_PASSWORD "7387842399"
-#define MQTT_SERVER "192.168.1.81"
-#define MQTT_PORT 1884
-#define CLIENT_USERNAME "esp32"
-#define CLIENT_ID "esp32id"
-#define CLIENT_PASS "hydroponika"
